6ef1df13ee46361dbd97c08c121b295c3021c697
Tom Schneider <05.tom.schneider@gmail.com>
2025-09-04 17:23:05 +0300

    add bookmark search

--- search.js
+++ search.js
@@ -1,12 +1,16 @@
 let relevantTabInfo = [];
+relevantBookmarkInfo = [];
 let searchBoxElem = document.querySelector(".search-box");
 searchBoxElem.focus();
-let selectedTabInOrder = 0;
+let selectedTabFromList;
 const tabListElem = document.querySelector(".tab-list");
+const selectionIndicatorClass = "tab--selected";
 
 // TODO:
-// - search for bookmarks
 // - handle missing favicons
+// - fix scrollbar styling
+// - display for which tab is currently active in browser
+// - fix selection of last element (jumping over divider)
 
 function getTabsOfCurrentWindow() {
   return browser.tabs.query({ currentWindow: true });
@@ -18,12 +22,11 @@ browser.tabs.onRemoved.addListener((tabIdPedingRemoval) => {
   updateList(tabIdPedingRemoval); // this is needed, because the event fires to early
 });
 
-function updateRelevantInfo(ignoreIdOfPendingRemoval = null) {
+function updateTabInfo(ignoreIdOfPendingRemoval = null) {
   return new Promise((resolve) => {
     getTabsOfCurrentWindow().then((tablist) => {
       relevantTabInfo = tablist
         .filter((tab) => {
-          console.log(ignoreIdOfPendingRemoval);
           return !(tab.id === ignoreIdOfPendingRemoval); // both null-ids and the pendingRemovalId should be ignored
         })
         .map((tab) => {
@@ -39,10 +42,37 @@ function updateRelevantInfo(ignoreIdOfPendingRemoval = null) {
   });
 }
 
+function updateBookmarkInfo() {
+  return new Promise((resolve) => {
+    browser.bookmarks.search({}).then((bookmarkInfo) => {
+      relevantBookmarkInfo = bookmarkInfo
+        .filter((bookmarkInfo) => {
+          return bookmarkInfo.type === "bookmark";
+        })
+        .map((bookmarkInfo) => {
+          return {
+            id: bookmarkInfo.id,
+            title: bookmarkInfo.title,
+            url: bookmarkInfo.url,
+          };
+        });
+
+      // console.log(relevantBookmarkInfo.length);
+      resolve();
+    });
+  });
+}
+
+function updateRelevantInfo(ignoreIdOfPendingRemoval = null) {
+  return Promise.all([
+    updateTabInfo(ignoreIdOfPendingRemoval),
+    updateBookmarkInfo(),
+  ]);
+}
+
 function handleSearchQueryChange() {
-  selectedTabInOrder = 0;
   createList(searchBoxElem.value);
-  updateSelection();
+  selectFirstTab();
 }
 
 searchBoxElem.addEventListener("input", handleSearchQueryChange);
@@ -54,18 +84,39 @@ searchBoxElem.addEventListener("keydown", (ev) => {
 });
 
 function selectPreviousTabInOrder() {
-  if (selectedTabInOrder > 0) {
-    selectedTabInOrder--;
+  if (!!!selectedTabFromList) {
+    // this shouldn't happen
+  } else if (!!selectedTabFromList.previousElementSibling) {
+    let previousTab = selectedTabFromList.previousElementSibling;
+    if (!isValidTabElement(previousTab)) {
+      previousTab = previousTab.previousElementSibling;
+      // this assumes that there is a maximum of one invalid element that should not be the last element
+    }
+    selectTabFromList(previousTab);
   } else {
-    selectedTabInOrder = getTabsByQuery(searchBoxElem.value).length - 1;
+    selectTabFromList(tabListElem.lastElementChild);
   }
 }
 
+function isValidTabElement(possibleTabElement) {
+  return (
+    !!possibleTabElement.dataset.tabId ||
+    !!possibleTabElement.dataset.bookmarkId
+  );
+}
+
 function selectNextTabInOrder() {
-  if (selectedTabInOrder < getTabsByQuery(searchBoxElem.value).length - 1) {
-    selectedTabInOrder++;
+  if (!!!selectedTabFromList) {
+    // this shouldn't happen
+  } else if (!!selectedTabFromList.nextElementSibling) {
+    let nextTab = selectedTabFromList.nextElementSibling;
+    // this assumes that there is a maximum of one invalid element that should not be the last element
+    if (!isValidTabElement(nextTab)) {
+      nextTab = nextTab.nextElementSibling;
+    }
+    selectTabFromList(nextTab);
   } else {
-    selectedTabInOrder = 0;
+    selectTabFromList(tabListElem.firstChild);
   }
 }
 
@@ -77,7 +128,6 @@ document.addEventListener("keydown", (ev) => {
     } else {
       selectNextTabInOrder();
     }
-    updateSelection();
   }
 });
 
@@ -93,35 +143,59 @@ function getTabsByQuery(searchQuery) {
   });
 }
 
+function getBookmarksByQuery(searchQuery) {
+  const lowercaseSearchQuery = searchQuery.toLowerCase();
+
+  return relevantBookmarkInfo.filter((bookmarkInfo) => {
+    return (
+      (!!bookmarkInfo.url &&
+        bookmarkInfo.url.toLowerCase().includes(lowercaseSearchQuery)) ||
+      (bookmarkInfo.title &&
+        bookmarkInfo.title.toLowerCase().includes(lowercaseSearchQuery))
+    );
+  });
+}
+
 function createList(searchQuery) {
   const tabList = getTabsByQuery(searchQuery).map((tabInfo) => {
-    return createElementFrom(tabInfo);
+    return createTabDisplayFrom(tabInfo);
   });
-  tabListElem.innerHTML = tabList.join("");
+  const bookmarkList = getBookmarksByQuery(searchQuery).map((bookmarkInfo) => {
+    return createBookmarkDisplayFrom(bookmarkInfo);
+  });
+
+  tabListElem.innerHTML = `
+  ${tabList.join("")}
+  <div class="divider-line"></div>
+  ${bookmarkList.join("")}
+  `;
 }
 
 document.addEventListener("keypress", (ev) => {
   if (ev.key == "Enter") {
-    const selectedTabEntry = getTabsByQuery(searchBoxElem.value)[
-      selectedTabInOrder
-    ];
-    if (selectedTabEntry == undefined) return;
-    const id = selectedTabEntry.id;
-    if (id == undefined) return;
-
-    // console.log("switching tab");
-
-    browser.tabs.update(id, {
-      active: true,
-    });
-
-    window.close();
+    let selectedElement = document.querySelector("." + selectionIndicatorClass);
+    if (!!!selectedElement == undefined) return;
+    const tabId = selectedElement.dataset.tabId;
+    const bookmarkId = selectedElement.dataset.bookmarkId;
+    if (!!tabId) {
+      browser.tabs.update(parseInt(tabId), {
+        active: true,
+      });
+      window.close();
+      return;
+    } else if (!!bookmarkId) {
+      // get url by bookmarkId
+      browser.bookmarks.get(bookmarkId).then((bookmarkTreeNode) => {
+        browser.tabs.create({ url: bookmarkTreeNode[0].url, active: true });
+        window.close();
+      });
+    }
   }
 });
 
-function createElementFrom(tabInfo) {
+function createTabDisplayFrom(tabInfo) {
   return `
-    <div class="tab-display">
+    <div class="tab-display" data-tab-id="${tabInfo.id}">
         <img class="tab-icon" src=${tabInfo.favIconUrl}>
         <span class="tab-text">${tabInfo.title}</span>
         <div class="tab-key-hint">↵</div>
@@ -129,21 +203,44 @@ function createElementFrom(tabInfo) {
     `;
 }
 
-const selectionIndicatorClass = "tab--selected";
-function updateSelection() {
+function createBookmarkDisplayFrom(bookmarkInfo) {
+  return `
+    <div class="tab-display" data-bookmark-id="${bookmarkInfo.id}">
+        <svg class="tab-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143Zm126 18L314-169q-11 7-23 6t-21-8q-9-7-14-17.5t-2-23.5l44-189-147-127q-10-9-12.5-20.5T140-571q4-11 12-18t22-9l194-17 75-178q5-12 15.5-18t21.5-6q11 0 21.5 6t15.5 18l75 178 194 17q14 2 22 9t12 18q4 11 1.5 22.5T809-528L662-401l44 189q3 13-2 23.5T690-171q-9 7-21 8t-23-6L480-269Zm0-201Z"/></svg>
+        <span class="tab-text">${bookmarkInfo.title}</span>
+        <div class="tab-key-hint">↵</div>
+    </div>
+    `;
+}
+
+function clearTabSelection() {
+  // would there be any benefit in using querySelector and querying for the class?
   const tabArray = Array.from(tabListElem.children);
-  tabArray.forEach((child, index) => {
+  tabArray.forEach((child) => {
     child.classList.remove(selectionIndicatorClass);
-    if (index == selectedTabInOrder) {
-      child.classList.add(selectionIndicatorClass);
-    }
   });
+}
+
+function selectTabFromList(tab) {
+  clearTabSelection();
+  selectedTabFromList = tab;
+  // console.log("tab: " + selectedTabFromList);
+  selectedTabFromList.classList.add(selectionIndicatorClass);
+  selectedTabFromList.scrollIntoView({ block: "nearest" });
+}
+
+function selectFirstTab() {
+  selectTabFromList(tabListElem.firstElementChild);
 }
 
 function updateList(ignoreIdOfPendingRemoval = null) {
   updateRelevantInfo(ignoreIdOfPendingRemoval).then(() => {
     createList("");
-    updateSelection();
+    console.log("length: " + tabListElem.children.length);
+    console.log(
+      "first-child: " + JSON.stringify(tabListElem.firstElementChild)
+    );
+    selectFirstTab();
   });
 }
 
