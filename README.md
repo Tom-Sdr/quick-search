210d29e562148ce1330ed7e677704c6a7c1037cd
Tom Schneider <05.tom.schneider@gmail.com>
2026-01-06 22:30:04 +0100

    fix script injection vulnerability through title

--- README.md
+++ README.md
@@ -6,7 +6,23 @@ Easy way to run without taking too much space and immediately opening browser co
 `web-ext run --arg="--width=800" --arg="--height=800" --browser-console`
 It's recommended to set an alias for this. For example `wr`
 
+### If this doesn't work under ubuntu
+
+this may not work in ubuntu. just uninstall the snap version of firefox and install either the deb package from the website (i think this should work) or do this (https://askubuntu.com/a/1404401)
+
+## Build
+
+To build the project and get an installable version, run 
+```
+web-ext sign --api-key="user:{the-user-id}" --api-secret="{the-api-secret}" --channel="unlisted"
+```
+Note that the signing process can take several minutes.
+
 ## Install
 
 Install form the `.xpi`-file.
 To update it, a new version has to be signed.
+
+## Docs
+
+I had the problem of websites being able to inject javscript as script tags when setting their title to some malicious code. I fixed it by using templates and hydration via `element.textContent`. To verify this, the following snippet `document.title = '<div style="width: 10rem; height: 10rem; background: red;"></div>'` can quickly be used to set such a malicious title. This should now be safe and not display a red square in the menu. The same applies when such a page is bookmarked.
--- search.js
+++ search.js
@@ -7,8 +7,6 @@ const tabListElem = document.querySelector(".tab-list");
 const selectionIndicatorClass = "tab--selected";
 const tabActiveClass = "tab-display--active";
 
-// FIXME: sanitize tab-title! something like <canvas> in the title will actually be inserted into the dom because of innerHTML=...
-
 // TODO:
 // - handle missing favicons
 // - display for bookmark-folders -> tree view by default? enter to show?
@@ -167,19 +165,24 @@ function getBookmarksByQuery(searchQuery) {
   });
 }
 
+const dividerLineTemplate = document.createElement("template");
+dividerLineTemplate.innerHTML = `
+  <div class="divider-line"></div>
+`;
+
 function createList(searchQuery) {
-  const tabList = getTabsByQuery(searchQuery).map((tabInfo) => {
-    return createTabDisplayFrom(tabInfo);
-  });
-  const bookmarkList = getBookmarksByQuery(searchQuery).map((bookmarkInfo) => {
-    return createBookmarkDisplayFrom(bookmarkInfo);
+  tabListElem.innerHTML = "";
+  
+  getTabsByQuery(searchQuery).forEach((tabInfo) => {
+    tabListElem.appendChild(createTabDisplayFrom(tabInfo));
   });
 
-  tabListElem.innerHTML = `
-  ${tabList.join("")}
-  <div class="divider-line"></div>
-  ${bookmarkList.join("")}
-  `;
+  const dividerLine = dividerLineTemplate.content.firstElementChild.cloneNode(true);
+  tabListElem.appendChild(dividerLine);
+  
+  getBookmarksByQuery(searchQuery).forEach((bookmarkInfo) => {
+    tabListElem.appendChild(createBookmarkDisplayFrom(bookmarkInfo));
+  });
 }
 
 document.addEventListener("keypress", (ev) => {
@@ -204,24 +207,42 @@ document.addEventListener("keypress", (ev) => {
   }
 });
 
+const tabDisplayTemplate = document.createElement("template");
+tabDisplayTemplate.innerHTML = `
+  <div class="tab-display">
+      <img class="tab-icon">
+      <span class="tab-text"></span>
+      <div class="tab-key-hint">↵</div>
+  </div>
+`;
+
 function createTabDisplayFrom(tabInfo) {
-  return `
-    <div class="tab-display" data-tab-id="${tabInfo.id}">
-        <img class="tab-icon" src=${tabInfo.favIconUrl}>
-        <span class="tab-text">${tabInfo.title}</span>
-        <div class="tab-key-hint">↵</div>
-    </div>
-    `;
+  const tabDisplayInstance = tabDisplayTemplate.content.firstElementChild.cloneNode(true);
+
+  tabDisplayInstance.dataset.tabId = tabInfo.id;
+  tabDisplayInstance.querySelector(".tab-icon").src = tabInfo.favIconUrl || "";
+  tabDisplayInstance.querySelector(".tab-text").textContent = tabInfo.title;
+
+  return tabDisplayInstance;
 }
 
+
+const bookmarkDisplayTemplate = document.createElement("template");
+bookmarkDisplayTemplate.innerHTML = `
+  <div class="tab-display">
+      <svg class="tab-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143Zm126 18L314-169q-11 7-23 6t-21-8q-9-7-14-17.5t-2-23.5l44-189-147-127q-10-9-12.5-20.5T140-571q4-11 12-18t22-9l194-17 75-178q5-12 15.5-18t21.5-6q11 0 21.5 6t15.5 18l75 178 194 17q14 2 22 9t12 18q4 11 1.5 22.5T809-528L662-401l44 189q3 13-2 23.5T690-171q-9 7-21 8t-23-6L480-269Zm0-201Z"/></svg>
+      <span class="tab-text"></span>
+      <div class="tab-key-hint">↵</div>
+  </div>
+`;
+
 function createBookmarkDisplayFrom(bookmarkInfo) {
-  return `
-    <div class="tab-display" data-bookmark-id="${bookmarkInfo.id}">
-        <svg class="tab-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143Zm126 18L314-169q-11 7-23 6t-21-8q-9-7-14-17.5t-2-23.5l44-189-147-127q-10-9-12.5-20.5T140-571q4-11 12-18t22-9l194-17 75-178q5-12 15.5-18t21.5-6q11 0 21.5 6t15.5 18l75 178 194 17q14 2 22 9t12 18q4 11 1.5 22.5T809-528L662-401l44 189q3 13-2 23.5T690-171q-9 7-21 8t-23-6L480-269Zm0-201Z"/></svg>
-        <span class="tab-text">${bookmarkInfo.title}</span>
-        <div class="tab-key-hint">↵</div>
-    </div>
-    `;
+  const bookmarkDisplayInstance = bookmarkDisplayTemplate.content.firstElementChild.cloneNode(true);
+
+  bookmarkDisplayInstance.dataset.bookmarkId = bookmarkInfo.id;
+  bookmarkDisplayInstance.querySelector(".tab-text").textContent = bookmarkInfo.title;
+
+  return bookmarkDisplayInstance;
 }
 
 function clearTabSelection() {
